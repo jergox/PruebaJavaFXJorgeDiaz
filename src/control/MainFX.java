2b7837e9c96aaaf2fa8e28e7fea2259b719341f4
Usuario DAM <Usuario DAM@LAPTOP-1JS68G34.alumnos.uem.es>
2017-12-05 17:48:24 +0100

    Primer commit: Funciona y diseño

---
+++ src/control/MainFX.java
@@ -0,0 +1,53 @@
+package control;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class MainFX extends Application {
+	
+	Stage stage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			/*
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			*/
+			this.stage=primaryStage;
+
+	        MainWindow();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void MainWindow() {
+		
+		try {
+
+        	FXMLLoader loader=new FXMLLoader(getClass().getResource("../vista/Login.fxml"));
+            AnchorPane pane=loader.load();
+            Scene scene=new Scene(pane);
+            stage.setScene(scene);
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
